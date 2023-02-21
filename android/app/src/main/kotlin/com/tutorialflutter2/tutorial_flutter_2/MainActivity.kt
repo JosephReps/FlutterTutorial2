03b780eb156a80f1b3219a0d16ec0104759415a8
Jrepax <joseph.reps@gmail.com>
2023-02-21 16:49:42 +1300

    Initial commit

---
+++ android/app/src/main/kotlin/com/tutorialflutter2/tutorial_flutter_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tutorialflutter2.tutorial_flutter_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tutorial_flutter_2/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register")),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                return const Text("Done (verified).");
+              } else {
+                return const Text("Done (NOT).");
+              }
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
